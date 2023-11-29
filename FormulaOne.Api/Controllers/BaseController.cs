a39686e33b2fe53f8a53097851def3b50224d17e
Irshad Abdul <er.abdulirshad@gmail.com>
2023-11-29 12:12:18 +0530

    update base controller

--- FormulaOne.Api/Controllers/BaseController.cs
+++ FormulaOne.Api/Controllers/BaseController.cs
@@ -6,15 +6,9 @@ namespace FormulaOne.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BaseController : ControllerBase
+    public class BaseController(IUnitOfWork unitOfWork, IMapper mapper) : ControllerBase
     {
-        protected readonly IUnitOfWork _unitOfWork;
-        protected readonly IMapper _mapper;
-
-        public BaseController(IUnitOfWork unitOfWork, IMapper mapper)
-        {
-            _unitOfWork = unitOfWork;
-            _mapper = mapper;
-        }
+        protected readonly IUnitOfWork _unitOfWork = unitOfWork;
+        protected readonly IMapper _mapper = mapper;
     }
 }
