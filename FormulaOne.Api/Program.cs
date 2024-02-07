33ec81defb565cd78a4f1fec07c337dfbc1bbd53
Irshad Abdul <er.abdulirshad@gmail.com>
2024-02-07 22:22:36 +0530

    Delete unnecessary files and configurations

--- FormulaOne.Api/Program.cs
+++ FormulaOne.Api/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
