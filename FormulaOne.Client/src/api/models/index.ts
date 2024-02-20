60f4b7c2459c272d79dff21d7cee267267bb9650
Irshad Abdul <er.abdulirshad@gmail.com>
2024-02-20 12:31:33 +0530

    Add OpenAPI generator files and models

---
+++ FormulaOne.Client/src/api/models/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateDriverAchievementRequest';
+export * from './CreateDriverRequest';
+export * from './UpdateDriverAchievementRequest';
+export * from './UpdateDriverRequest';
