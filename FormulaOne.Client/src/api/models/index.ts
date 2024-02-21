e308cb09910e3e4bb4018cc43e6af3727a064891
Irshad Abdul <er.abdulirshad@gmail.com>
2024-02-21 11:45:41 +0530

    Delete unused API and model files

--- FormulaOne.Client/src/api/models/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './CreateDriverAchievementRequest';
-export * from './CreateDriverRequest';
-export * from './UpdateDriverAchievementRequest';
-export * from './UpdateDriverRequest';
